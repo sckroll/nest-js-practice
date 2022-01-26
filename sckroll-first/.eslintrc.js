29dd3b80cbc6c8c969a4059a0894b4b9fc216106
김성찬 <kimsc0714@naver.com>
2022-01-26 23:39:01 +0900

    chore: Prettier 설정을 ESLint 파일에 통합

--- sckroll-first/.eslintrc.js
+++ sckroll-first/.eslintrc.js
@@ -20,5 +20,19 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: true,
+        useTabs: false,
+        tabWidth: 2,
+        trailingComma: 'all',
+        printWidth: 80,
+        bracketSpacing: true,
+        arrowParens: 'avoid',
+        endOfLine: 'auto',
+      },
+    ],
   },
 };
