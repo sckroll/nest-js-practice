0775939309aba9923c19780bfff0abcf1f0c229a
김성찬 <kimsc0714@naver.com>
2022-01-30 18:24:41 +0900

    feat: add signup service

--- sckroll-cats/src/cats/cats.controller.ts
+++ sckroll-cats/src/cats/cats.controller.ts
@@ -1,16 +1,18 @@
-import { CatsService } from './cats.service';
 import {
+  Body,
   Controller,
   Get,
   Post,
   UseFilters,
   UseInterceptors,
 } from '@nestjs/common';
+import { CatsService } from './cats.service';
 import { HttpExceptionFilter } from 'src/common/exceptions/http-exception.filter';
 import { SuccessInterceptor } from 'src/common/interceptors/success.interceptor';
+import { CatRequestDto } from './dto/cats.request.dto';
 
 @Controller('cats')
-@UseInterceptors(new SuccessInterceptor())
+@UseInterceptors(SuccessInterceptor)
 @UseFilters(HttpExceptionFilter)
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
@@ -21,8 +23,8 @@ export class CatsController {
   }
 
   @Post()
-  async signUp() {
-    return 'signup';
+  async signUp(@Body() body: CatRequestDto) {
+    return await this.catsService.signUp(body);
   }
 
   @Post('login')
--- sckroll-cats/src/cats/cats.service.ts
+++ sckroll-cats/src/cats/cats.service.ts
@@ -1,4 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import {
+  HttpException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import * as bcrypt from 'bcrypt';
+import { Cat } from './cats.schema';
+import { CatRequestDto } from './dto/cats.request.dto';
 
 @Injectable()
-export class CatsService {}
+export class CatsService {
+  constructor(@InjectModel(Cat.name) private catModel: Model<Cat>) {}
+
+  async signUp(body: CatRequestDto) {
+    const { email, name, password } = body;
+
+    // 1. 유효성 검사: 고양이 존재 유무 확인
+    const isCatExist = await this.catModel.exists({ email });
+    if (isCatExist) {
+      // throw new HttpException('해당하는 고양이는 이미 존재합니다.', 401);
+      throw new UnauthorizedException('해당하는 고양이는 이미 존재합니다.');
+    }
+
+    // 2. 패스워드 암호화
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // 3. DB 저장
+    const cat = await this.catModel.create({
+      email,
+      name,
+      password: hashedPassword,
+    });
+
+    return cat.readOnlyData;
+  }
+}
