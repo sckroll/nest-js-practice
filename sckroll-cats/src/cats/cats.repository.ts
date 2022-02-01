716d99783693bc408781e970f15268e8d2221e2e
김성찬 <kimsc0714@naver.com>
2022-02-01 23:14:27 +0900

    refactor: apply repository pattern

---
+++ sckroll-cats/src/cats/cats.repository.ts
@@ -0,0 +1,26 @@
+import { Injectable, HttpException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Cat } from './cats.schema';
+import { CatRequestDto } from './dto/cats.request.dto';
+
+@Injectable()
+export class CatsRepository {
+  constructor(@InjectModel(Cat.name) private readonly catModel: Model<Cat>) {}
+
+  // 사실 Mongoose 단에서 에러 처리를 해주기 때문에
+  // 아래처럼 에러 처리 코드를 추가해줄 필요는 없지만
+  // 항상 처리가 잘 된다는 보장은 없음
+  async existsByEmail(email: string): Promise<boolean> {
+    try {
+      const result = await this.catModel.exists({ email });
+      return result;
+    } catch (error) {
+      throw new HttpException('db error', 400);
+    }
+  }
+
+  async create(cat: CatRequestDto): Promise<Cat> {
+    return await this.catModel.create(cat);
+  }
+}
--- sckroll-cats/src/cats/cats.service.ts
+++ sckroll-cats/src/cats/cats.service.ts
@@ -1,3 +1,4 @@
+import { CatsRepository } from './cats.repository';
 import {
   HttpException,
   Injectable,
@@ -11,13 +12,15 @@ import { CatRequestDto } from './dto/cats.request.dto';
 
 @Injectable()
 export class CatsService {
-  constructor(@InjectModel(Cat.name) private catModel: Model<Cat>) {}
+  // constructor(@InjectModel(Cat.name) private catModel: Model<Cat>) {}
+  constructor(private readonly catsRepository: CatsRepository) {}
 
   async signUp(body: CatRequestDto) {
     const { email, name, password } = body;
 
     // 1. 유효성 검사: 고양이 존재 유무 확인
-    const isCatExist = await this.catModel.exists({ email });
+    // const isCatExist = await this.catModel.exists({ email });
+    const isCatExist = await this.catsRepository.existsByEmail(email);
     if (isCatExist) {
       // throw new HttpException('해당하는 고양이는 이미 존재합니다.', 401);
       throw new UnauthorizedException('해당하는 고양이는 이미 존재합니다.');
@@ -27,7 +30,12 @@ export class CatsService {
     const hashedPassword = await bcrypt.hash(password, 10);
 
     // 3. DB 저장
-    const cat = await this.catModel.create({
+    // const cat = await this.catModel.create({
+    //   email,
+    //   name,
+    //   password: hashedPassword,
+    // });
+    const cat = await this.catsRepository.create({
       email,
       name,
       password: hashedPassword,
